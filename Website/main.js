8a7965653d79314e13d4ed65ac7ab0e451dc1102
David-Diniz <79600133+David-Diniz@users.noreply.github.com>
2024-05-09 18:58:59 -0400

    Trying to make text appear with button press

--- Website/main.js
+++ Website/main.js
@@ -5,3 +5,10 @@ function doSomething() {
     alert('did a thing!')
 }
 
+// function main(){
+//     // alert("String go here")
+//     var mydiv = document.getElementById("thing")
+//     console.log(mydiv)
+//     mydiv.innerText = "Stringy"
+// }
+// window.onload = main
